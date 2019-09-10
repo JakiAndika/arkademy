f294904b856efb09f79b7cf736241a0a0b02da8a
Mohamad Jaki Andika 80 <jakiandika19@gmail.com>
2019-09-10 09:12:04 +0700

    belajarFS

---
+++ minggu-8/cobaFS.js
@@ -0,0 +1,23 @@
+//Proses pemanggilan library fs
+const fs = require('fs')
+
+console.log('start reading a file...')
+
+//Proses readfile text.txt menggunakan library fs
+fs.readFile('text.txt', 'utf-8', (err,content) => {
+    if(err) {
+        console.log('error happened during reading the file')
+        return console.log(err)
+    }
+
+    //Lakukan sesuatu disini kalau berhasil
+
+    //jika berhasil rename text.txt ke terserah.txt
+    fs.rename('text.txt', 'terserah.txt', (err) => {
+        if(err) console.log("Gagal rename!"+err);
+
+        console.log("Berhasil")
+    })
+
+})
+console.log('end of the file')
