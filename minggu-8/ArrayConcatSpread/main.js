23f387b39b00615f030101c2580ee9e047ec5cd2
Mohamad Jaki Andika 80 <jakiandika19@gmail.com>
2019-09-12 09:15:21 +0700

    ArrayConcatSpread

---
+++ minggu-8/ArrayConcatSpread/main.js
@@ -0,0 +1,13 @@
+const arr1 = [1, 2, 3 ]; 
+const arr2 = [4, 5, 6 ]; 
+
+const obj1 = {name: "coba", age: 25};
+const obj2 = {address: "Jakarta", gender: "laki-laki"}
+
+//Penggabungan object menggunakan spread operator
+const merge = {...obj1,...obj2};
+
+/*Console log array concat, untuk menampilkan penggabungan
+arr1 & arr2 */
+console.log(arr1.concat(arr2));
+console.log(merge);
