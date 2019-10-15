448f89d4c880ecb0d354740f5af6f6a09cda31b4
Mohamad Jaki Andika 80 <jakiandika19@gmail.com>
2019-10-15 09:03:19 +0700

    minggu-11/reactJs

--- minggu-10/my-app/src/App.js
+++ minggu-10/my-app/src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
-import Header from "./Header";
 
-function App() {
-  return (
-    <div className="App">
-      <Header title="Header" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  };
+  handleAdd() {
+    if (this.state.count < 10) {
+      this.setState({ count: this.state.count + 1 });
+    }
+  }
+  handleMin() {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 });
+    }
+  }
+  handleReset() {
+    this.setState({ count: 0 });
+  }
+  render() {
+    return(
+      <div className="body" >
+        <p>{this.state.count}</p>
+        <button onClick={() => this.handleAdd()}>+</button>
+        <button onClick={() => this.handleMin()}>-</button>
+        <button onClick={() => this.handleReset()}>0</button>
+      </div>
+    );
+  }
 }
 
 export default App;
